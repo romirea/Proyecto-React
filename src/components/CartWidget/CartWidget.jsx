31e80a142c6f01149728e21917b737cf0768e9ca
rominarea <rominarea@gmail.com>
2023-07-17 21:51:35 -0300

    icono carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -3,8 +3,8 @@ import { FaShoppingCart } from "react-icons/fa";
 
 const CartWidget = () => {
     return (
-        <a className="nav-link" href="#">
-            <FaShoppingCart size={25}/> <span className="cart-quantity">3</span>
+        <a className="nav-link" href="/cart">
+            <FaShoppingCart size={25}/> <span className="cart-quantity" ></span>
         </a>
     );
 };
